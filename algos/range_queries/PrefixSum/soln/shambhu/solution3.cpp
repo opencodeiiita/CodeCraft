bab0236e38e62fd3e7e374dce72d16f928ccdef5
Zarxeon <shambhuchadana04@gmail.com>
2025-12-28 22:53:17 +0530

    Create solution3.cpp

---
+++ algos/range_queries/PrefixSum/soln/shambhu/solution3.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const ll MOD = 998244353;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    // cout << "n, s: " << n << " " << s << endl;
+
+    int st = 0, en = n-1;
+    vector<int> ones, zeroes;
+    while (st < en) {
+        // cout << st << " -=- " << en << endl;
+        bool oneFound = false, zeroFound = false;
+        while (st < n && s[st] == '0') {
+            st++;
+        }
+        if (st < n) oneFound = true;
+        // cout << "found one at " << st << endl;
+        while (en >= 0 && s[en] == '1') {
+            en--;
+        }
+        if (en >= 0) zeroFound = true;
+        // cout << "found zero at " << en << endl;
+        // cout << oneFound << " " << zeroFound << endl;
+        if (oneFound && zeroFound) {
+            // cout << "meow\n";
+            if (st < en) {
+                // cout << "yes\n";
+                ones.push_back(st++ + 1);
+                zeroes.push_back(en-- + 1);
+                continue;
+            }
+            else break;
+        }
+        else {
+            break;
+        }
+    }
+
+    if (!ones.size()) {
+        cout << 0 << endl;
+        return;
+    }
+
+    reverse(zeroes.begin(), zeroes.end());
+
+    cout << 1 << endl;
+    cout << (2 * ones.size()) << " ";
+    for (auto el: ones) cout << el << " ";
+    for (auto el: zeroes) cout << el << " ";
+
+    cout << endl;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
